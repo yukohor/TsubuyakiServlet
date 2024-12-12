cac4efd93faf9f936a728bb336a9dc389810360f
ユーザー名yuko <ka91001xxx@yahoo.co.jp>
2024-12-12 21:28:37 +0900

    12/12　つぶやき機能

---
+++ src/main/java/servlet/Main.java
@@ -0,0 +1,80 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Mutter;
+import model.PostMutterLogic;
+import model.User;
+
+@WebServlet("/Main2")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//つぶやきリストをアプリケーションスコープから取得
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+		//取得できなかった場合は、つぶやきリストを新規登録して
+		//アプリケーションスコープに保存
+		if (mutterList == null) {
+			mutterList = new ArrayList<>();
+			application.setAttribute("mutterList", mutterList);
+		}
+
+		//ログインしているか確認するため
+		//セッションスコープからユーザー情報を取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		if (loginUser == null) { //ログインしていない場合
+			//リダイレクト
+			response.sendRedirect("index.jsp");
+		} else {//ログイン済みの場合
+			//フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//リクエストパラメーターの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		//入力チェック
+		if (text != null && text.length() != 0) {
+			//アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+
+			//セッションスコープに保存されたユーザー情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			//つぶやきを作成してつぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+
+			//アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList", mutterList);
+		}
+
+		//メイン画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="model.User, model.Mutter,java.util.List"%>
+<%
+//セッションスコープに保存されたユーザー情報を取得
+User loginUser = (User) session.getAttribute("loginUser");
+//アプリケーションスコープに保存されたつぶやきリストを取得
+List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>どこつぶ</title>
+</head>
+<body>
+	<h1>どこつぶメイン</h1>
+	<p>
+		<%=loginUser.getName()%>さん、ログイン中 <a href="Logout">ログアウト</a>
+	</p>
+	<p>
+		<a href="Main">更新</a>
+	</p>
+	<form action="Main" method="post">
+		<input type="text" name="text"> 
+		<input type="submit" value="つぶやく">
+	</form>
+	<%
+	for (Mutter mutter : mutterList) {
+	%>
+	<p><%= mutter.getUserName() %> : <%= mutter.getText() %></p>
+	<% } %>
+</body>
+</html>
